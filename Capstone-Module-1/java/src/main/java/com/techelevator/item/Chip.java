643f31845d31ee90382d6f6c9f0dacef957b677f
Christopher Nichols <chris.nichols.23@gmail.com>
2019-11-22 11:42:11 -0500

    Pushing Module 1 Capstone

---
+++ Capstone-Module-1/java/src/main/java/com/techelevator/item/Chip.java
@@ -0,0 +1,25 @@
+package com.techelevator.item;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.techelevator.FileReader;
+
+public class Chip extends Item {
+
+	private String noise;
+	private String slot;
+
+	public Chip(String slot) {
+		super(slot);
+		this.slot = slot;
+	}
+
+	public String getSlot() {
+		return slot;
+	}
+}
+ 
